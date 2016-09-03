d96a46c248d41fec74bcfc9ac2412b4e798d6b63
Francisco Gileno <gileno@gmail.com>
2016-09-03 09:56:57 -0300

    Updating to cra 0.4.0

--- control-panel/src/App.js
+++ control-panel/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Layout from './layout/Layout';
-import './App.css';
 
 class App extends Component {
   render() {
--- control-panel/src/index.js
+++ control-panel/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FirebaseApp from './FirebaseApp';
+import 'react-mdl/extra/material.js';
+import 'react-mdl/extra/material.css';
+import './App.css';
+
 
 ReactDOM.render(<FirebaseApp />, document.getElementById('root'));
